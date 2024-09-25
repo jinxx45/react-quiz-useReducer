c497d04d581cabc862f21dec6165e1a8ade2d777
Pragadeesh <www.pragat7@gmail.com>
2024-09-25 18:55:04 +0530

    bug fix- should not use import for images in public folder

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./Header.css";
-import ReactLogo from "../../../public/images/reactlogo.png";
 
 export default function Header() {
   return (
     <div className="header">
-      <img className="logo" src={ReactLogo} alt="React Logo" />
+      <img className="logo" src="/images/reactlogo.png" alt="React Logo" />
       <h1 className="header-text inria-sans-light">React Quiz</h1>
     </div>
   );
